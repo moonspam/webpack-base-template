ead75136fe376f74113b61698bb76731933dbf90
문성영 <moonspam@munseong-yeong-ui-MacBook-Pro.local>
2018-04-04 20:57:56 +0900

    add uglify js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
+const webpack = require('webpack'); // to access built-in plugins
 const path = require('path');
 
-module.exports = {
+const config = {
   entry: `${__dirname}/public/src/index.js`,
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public', 'dist'),
+    path: path.join(__dirname, 'public', 'dist'),
   },
   devServer: {
     inline: true,
-    contentBase: path.resolve(__dirname, 'public', 'dist'),
+    contentBase: path.join(__dirname, 'public', 'dist'),
     port: 7777,
   },
   module: {
@@ -33,10 +34,16 @@ module.exports = {
               targets: {
                 browsers: ['last 2 versions'],
               },
+              module: false, // Bundling 결과로부터 사용되지 않은 코드를 삭제하여 파일 크기 경량화
             },
           ]],
         },
       },
     ],
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
 };
+
+module.exports = config;
