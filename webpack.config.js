bac2bac89189f0e1d36a39e1e8c185495cc5513e
문성영 <moonspam@munseong-yeong-ui-MacBook-Pro.local>
2018-04-05 22:36:27 +0900

    add favicon, beautify

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ const outputPath = './public/dist/';
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlBeautifyPlugin = require('html-beautify-webpack-plugin');
 
 const config = {
   context: path.resolve(__dirname, sourcePath),
@@ -52,11 +54,25 @@ const config = {
   plugins: [
     new CleanWebpackPlugin([outputPath]),
     new ExtractTextPlugin('./css/styles.css'),
+    new FaviconsWebpackPlugin({
+      logo: './img/favicon.png',
+    }),
     new HtmlWebpackPlugin({
       template: './index.html',
       hash: true,
       inject: 'body',
       chunks: ['app'],
+    }),
+    new HtmlBeautifyPlugin({
+      config: {
+        html: {
+          indent_size: 2,
+          end_with_newline: true,
+          preserve_newlines: true,
+          unformatted: ['p', 'i', 'b', 'span'],
+        },
+      },
+      replace: [' type="text/javascript"'],
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.UglifyJsPlugin({
