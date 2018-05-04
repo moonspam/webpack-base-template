418bbc8dd1dbdf7fab402ec3645b38c583a181bf
moonspam <booboo0412@gmail.com>
2018-05-04 11:39:46 +0900

    html auto scraping + some fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
 
 const sourcePath = './public/src/';
 const outputPath = './public/dist/';
@@ -34,138 +35,141 @@ const siteInfo = {
   ],
 };
 
-// Webpack 플러그인
-const plugins = [
-  new CleanWebpackPlugin([outputPath]),
-  new ExtractTextPlugin('./css/styles.css'),
-  new FaviconsWebpackPlugin({
-    logo: './img/favicon.png',
-    emitStats: false,
-    icons: {
-      android: false,
-      appleIcon: true,
-      appleStartup: false,
-      coast: false,
-      favicons: true,
-      firefox: false,
-      windows: false,
-      yandex: false,
-    },
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false,
-    },
-  }),
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-  }),
-];
-
-// siteInfo.html 값의 개수에 따라 HtmlWebpackPlugin 생성
-const htmlList = siteInfo.html.map(file => (
-  new HtmlWebpackPlugin({
-    template: `./${file}.html`,
-    filename: `${file}.html`,
+function generateHtmlPlugins(templateDir) {
+  const templateFiles = fs.readdirSync(templateDir).filter(file => file.substr(-5) === '.html');
+  return templateFiles.map(file => new HtmlWebpackPlugin({
+    template: `./${file}`,
+    filename: `${file}`,
     hash: true,
     inject: 'body',
     chunks: ['app'],
-  })
-));
+  }));
+}
 
-// HtmlWebpackPlugin 확장 플러그인
-const htmlPlugins = [
-  new HtmlBeautifyPlugin({
-    config: {
-      html: {
-        indent_size: 2,
-        end_with_newline: true,
-        preserve_newlines: true,
-        unformatted: ['p', 'i', 'b', 'span'],
+module.exports = (env) => {
+  // Webpack 플러그인
+  const plugins = [
+    new CleanWebpackPlugin([outputPath]),
+    new ExtractTextPlugin('./css/styles.css'),
+    new FaviconsWebpackPlugin({
+      logo: './img/favicon.png',
+      emitStats: false,
+      icons: {
+        android: false,
+        appleIcon: true,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        windows: false,
+        yandex: false,
       },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ];
+
+  // html의 개수에 따라 HtmlWebpackPlugin 생성
+  const htmlList = generateHtmlPlugins(sourcePath);
+
+  // HtmlWebpackPlugin 확장 플러그인
+  const htmlPlugins = [
+    new HtmlBeautifyPlugin({
+      config: {
+        html: {
+          indent_size: 2,
+          end_with_newline: true,
+          preserve_newlines: true,
+          unformatted: ['p', 'i', 'b', 'span'],
+        },
+      },
+      replace: [
+        { test: '@@_title', with: siteInfo.title },
+        { test: '@@_description', with: siteInfo.description },
+        { test: '@@_keywords', with: siteInfo.description },
+        { test: '@@_author', with: siteInfo.author },
+        { test: '@@_og_locale', with: siteInfo.og.locale },
+        { test: '@@_og_url', with: siteInfo.og.url },
+        { test: '@@_og_type', with: siteInfo.og.type },
+        { test: '@@_og_img_url', with: siteInfo.og.img.url },
+        { test: '@@_og_img_type', with: siteInfo.og.img.type },
+        { test: '@@_og_img_width', with: siteInfo.og.img.width },
+        { test: '@@_og_img_height', with: siteInfo.og.img.height },
+        { test: '@@_og_img_alt', with: siteInfo.og.img.alt },
+      ],
+    }),
+  ];
+
+  return {
+    context: path.resolve(__dirname, sourcePath),
+    entry: {
+      app: './js/index.js',
     },
-    replace: [
-      { test: '@@_title', with: siteInfo.title },
-      { test: '@@_description', with: siteInfo.description },
-      { test: '@@_keywords', with: siteInfo.description },
-      { test: '@@_author', with: siteInfo.author },
-      { test: '@@_og_locale', with: siteInfo.og.locale },
-      { test: '@@_og_url', with: siteInfo.og.url },
-      { test: '@@_og_type', with: siteInfo.og.type },
-      { test: '@@_og_img_url', with: siteInfo.og.img.url },
-      { test: '@@_og_img_type', with: siteInfo.og.img.type },
-      { test: '@@_og_img_width', with: siteInfo.og.img.width },
-      { test: '@@_og_img_height', with: siteInfo.og.img.height },
-      { test: '@@_og_img_alt', with: siteInfo.og.img.alt },
-    ],
-  }),
-];
-
-const config = {
-  context: path.resolve(__dirname, sourcePath),
-  entry: {
-    app: './js/index.js',
-  },
-  output: {
-    filename: './js/[name].bundle.js',
-    path: path.resolve(__dirname, outputPath),
-  },
-  devServer: {
-    open: true,
-    contentBase: path.resolve(__dirname, outputPath),
-    watchContentBase: true,
-    inline: true,
-  },
-  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [{
-            loader: 'css-loader',
-            options: {
-              minimize: true,
+    output: {
+      filename: './js/[name].bundle.js',
+      path: path.resolve(__dirname, outputPath),
+    },
+    devServer: {
+      open: true,
+      contentBase: path.resolve(__dirname, outputPath),
+      watchContentBase: true,
+      inline: true,
+    },
+    devtool: env.NODE_ENV === 'development' ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            use: [{
+              loader: 'css-loader',
+              options: {
+                minimize: env.NODE_ENV === 'production',
+              },
+            }, {
+              loader: 'sass-loader',
+            }],
+            fallback: 'style-loader',
+            publicPath: '../',
+          }),
+        },
+        {
+          test: /\.(jpe?g|png|gif)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            name: () => {
+              if (env.NODE_ENV === 'development') {
+                return '[name].[ext]';
+              }
+              return '[hash].[ext]';
             },
-          }, {
-            loader: 'sass-loader',
-          }],
-          fallback: 'style-loader',
-          publicPath: '../',
-        }),
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/,
-        exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          name: () => {
-            if (process.env.NODE_ENV === 'development') {
-              return '[path][name].[ext]';
-            }
-            return '[hash].[ext]';
+            outputPath: './img/',
           },
-          outputPath: './img/',
         },
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['babel-preset-env'],
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
         },
-      },
-    ],
-  },
-  plugins: plugins.concat(htmlList).concat(htmlPlugins),
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+          },
+        },
+      ],
+    },
+    plugins: plugins.concat(htmlList).concat(htmlPlugins),
+  };
 };
-
-module.exports = config;
